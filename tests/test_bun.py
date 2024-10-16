f5d909b51db244bf1685b25d2f49d73ff553109d
Evgeniy Andreev <evgeniyandreev21@gmail.com>
2024-10-16 12:34:09 +0300

    first_commit

---
+++ tests/test_bun.py
@@ -0,0 +1,38 @@
+import allure
+from bun import Bun
+
+class TestBun:
+    @allure.title('Проверка инициализации булочки')
+    @allure.description('Тест проверяет, что булочка инициализируется с правильными данными')
+    def test_init_bun(self):
+        bun = Bun('Булочка', 100)
+        assert bun.get_name() == 'Булочка', f"Ожидается название булочки: Булочка"
+        assert bun.get_price() == 100, f"Ожидается цена булочки: 100"
+        print('\nТест пройден!')
+
+    @allure.title('Проверка инициализации булочки с неправильными данными')
+    @allure.description('Тест проверяет, что булочка инициализируется с неправильными данными')
+    def test_init_bun_with_wrong_data(self):
+        bun = Bun(100, 'Булочка')
+        assert bun.get_name() == 100, f"Ожидается название булочки: 100"
+        assert bun.get_price() == 'Булочка', f"Ожидается цена булочки: Булочка"
+        print('\nТест пройден!')
+
+    @allure.title('Проверка изменении названии булочки')
+    @allure.description('Тест проверяет, что можно изменить название булочки')
+    def test_name_change(self):
+        bun = Bun('Булочка', 100)
+        bun.name = 'Булка'
+        assert bun.get_name() == 'Булка', f"Ожидается название булочки: Булка"
+        print('\nТест пройден!')
+
+    @allure.title('Проверка изменении цены булочки')
+    @allure.description('Тест проверяет, что можно изменить цену булочки')
+    def test_price_change(self): #Изменение цены булочки
+        bun = Bun('Булочка', 100)
+        bun.price = 500
+        assert bun.get_price() == 500, f"Ожидается цена булочки: 5"
+        print('\nТест пройден!')
+
+
+
---
+++ tests/test_ingredient.py
@@ -0,0 +1,40 @@
+import allure
+import pytest
+from ingredient import Ingredient
+
+class TestIngredient:
+    @allure.title('Проверка инициализации ингридиента')
+    @allure.description('Тест проверяет, что ингридиент инициализируется с правильными данными')
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        ('Соус', 'Сырный', 100),
+        ('Начинка', 'Повидло', 200),
+    ])
+    def test_init_ingredient(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_type() == ingredient_type, f"Ожидается тип ингридиента: {ingredient_type}"
+        assert ingredient.get_name() == name, f"Ожидается название ингридиента: {name}"
+        assert ingredient.get_price() == price, f"Ожидается цена ингридиента: {price}"
+        print('\nТест пройден!')
+
+    @allure.title('Проверка получения типа ингридиента')
+    @allure.description('Тест проверяет, что можно получить тип ингридиента')
+    def test_type_change(self):
+        ingredient = Ingredient('Соус', 'Сырный', 100)
+        assert ingredient.get_type() == 'Соус', f"Ожидается тип ингридиента: Соус"
+        print('\nТест пройден!')
+
+    @allure.title('Проверка получения названия ингридиента')
+    @allure.description('Тест проверяет, что можно получить название ингридиента')
+    def test_name_change(self):
+        ingredient = Ingredient('Соус', 'Сырный', 100)
+        assert ingredient.get_name() == 'Сырный', f"Ожидается название ингридиента: Горчичный"
+        print('\nТест пройден!')
+
+    @allure.title('Проверка получения цены ингридиента')
+    @allure.description('Тест проверяет, что можно получить цену ингридиента')
+    def test_price_change(self):
+        ingredient = Ingredient('Соус', 'Сырный', 100)
+        assert ingredient.get_price() == 100, f"Ожидается цена ингридиента: 100"
+        print('\nТест пройден!')
+
+
